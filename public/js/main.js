f8c6047117f5ec1bb5e340f1fc696840e76c63c4
Devon Singh <devons1@uci.edu>
2017-05-20 19:13:59 -0700

    no message

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,68 @@
 /* functions for general use */
+
+/* this function returns the value associated with 'whichParam on the URL */ 
+
 function getURLParameters(whichParam)
 {
 	var pageURL = window.location.search.substring(1);
 	var pageURLVariables = pageURL.split('&');
-	for(var i = 0; i < pageURLVariables.length; i++)
-	{
-		var parameterName = pageURLVariables[i].split('=');
-		if(parameterName[0] == whichParam)
+	for(var i = 0; i < pageURLVariables[i].length; i++)
 		{
+		var parameterName = pageURLVariables[i].split('=');
+		if(parameterName[0] == whichParam){
 			return parameterName[1];
 		}
 	}
 }
 
 var username = getURLParameters('username');
-if('undefined' == typeof username || !username)
-{
+if('undefined' == typeof username || !username){
 	username = 'Anonymous_'+Math.floor(Math.random()*100);
 }
-$('#messages').append('<h4>' + username.fontcolor("lightblue") +'</h4>');
+
+var chat_room = 'One_Room';
 
 /* Connect to the socket server */
+
 var socket = io.connect();
 
-socket.on('log', function(array){
+socket.on('log',function(array){
 	console.log.apply(console,array);
+});
+
+socket.on('join_room_response',function(payload){
+	if(payload.result == 'fail'){
+		alert(payload.message);
+		return;
+	}
+	$('#messages').append('<p><font color="lime">+ '+payload.username+' joined!</font></p>');
+});
+
+socket.on('send_message_response',function(payload){
+	if(payload.result == 'fail'){
+		alert(payload.message);
+		return;
+	}
+	$('#messages').append('<p><font color="sky blue"><b>'+payload.username+' says:</font></b> '+payload.message.fontcolor("white")+'</p>');
+});
+
+
+
+function send_message(){
+	var payload = {};
+	payload.room = chat_room;
+	payload.username = username;
+	payload.message = $('#send_message_holder').val();
+		console.log('*** Client Log Message: \'send_message\' payload: '+JSON.stringify(payload));
+	socket.emit('send_message',payload);
+}	
+	
+
+$(function(){
+	var payload = {};
+	payload.room = chat_room;
+	payload.username = username;
+	
+	console.log('*** Client Log Message: \'join_room\' payload: '+JSON.stringify(payload));
+	socket.emit('join_room',payload);
 });
