5565709b5c9c1e01c94fa2ae618734493a235ff6
Devon Singh <devons1@uci.edu>
2017-05-29 17:06:19 -0700

    no message

--- server.js
+++ server.js
@@ -175,6 +175,86 @@ io.sockets.on('connection', function (socket) {
 	});
 	
 
+/* send_message command */
+	/* payload:
+	   {
+	 		'room': room to join,
+	  		'username': username of person joining 
+	 	}
+	 	join room_room_response:
+	 	{ 
+	 		'result: 'success',
+	 		'room': room joined,
+	 		'username: username that joined
+	 		'membership': nuber of people in the room including the new one
+	 		}
+	 		or 
+	 		{ 
+	 		'result: 'fail',
+	 		'message': failure message
+	 		}
+	 */
+
+	socket.on('send_message',function(payload) {
+		log('server received a command','send_message',payload);
+		if(('undefined' === typeof payload) || !payload) {
+			var error_message = 'send_message had no payload, command aborted';
+			log(error_message);
+			socket.emit('send_message_response',  { 
+													result: 'fail',
+													message: error_message
+												   });
+			return;
+		}
+		
+		var room = payload.room;
+		if(('undefined' === typeof room) || !room) {
+			var error_message = 'send_message did not specify a room, command aborted';
+			log(error_message);
+			socket.emit('send_message_response',   { 
+													result: 'fail',
+													message: error_message
+													});
+			return;
+		}
+		
+		var username = payload.username;
+		if(('undefined' === typeof username) || !username) {
+			var error_message = 'send_message did not specify a username, command aborted';
+			log(error_message);
+			socket.emit('send_message_response',   { 
+													result: 'fail',
+													message: error_message
+													});
+			return;
+		}
+		
+		
+		var message = payload.message;
+		if(('undefined' === typeof message) || !message) {
+			var error_message = 'send_message did not specify a message, command aborted';
+			log(error_message);
+			socket.emit('send_message_response',   { 
+													result: 'fail',
+													message: error_message
+													});	
+			return;
+		}
+		
+		var success_data = {
+									result: 'success',
+									room: room,
+									username: username,
+									message: message
+							};
+							
+		io.sockets.in(room).emit('send_message_response',success_data);
+		log('Message sent to room ' + room + ' by ' + username + 'successfully' +JSON.stringify(success_data));		
+					
+	});
+
+
+
 	/* invite command */
 	/* payload:
 	   {
