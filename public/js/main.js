6afbb3d974c41b44c03083e040b39a3587cdd4a4
Devon Singh <devons1@uci.edu>
2017-06-10 16:37:06 -0700

    no message

--- public/js/main.js
+++ public/js/main.js
@@ -455,7 +455,9 @@ socket.on('game_update',function(payload){
 				$('#'+row+'_'+column).removeClass('hovered_over');
 
 				if(payload.game.whose_turn === my_color){
+					/* CUSTOM GAME - Removing this makes it so you can play tokens anywhere, even ontop existing ones
 						if(payload.game.legal_moves[row][column] === my_color.substr(0,1)){ 
+					*/
 							$('#'+row+'_'+column).addClass('hovered_over');
 							$('#'+row+'_'+column).click(function(r,c){
 								return function(){
@@ -467,8 +469,9 @@ socket.on('game_update',function(payload){
 									socket.emit('play_token',payload);
 								};
 							}(row,column));
+					/* CUSTOM GAME - Removing this makes it so you can play tokens anywhere, even ontop existing ones
 						}
-					
+					*/
 		
 				}
 				
