f67aa877380e4022b1ad3a741e01c0dc57081c3e
Devon Singh <devons1@uci.edu>
2017-06-10 19:10:27 -0700

    no message

--- public/js/main.js
+++ public/js/main.js
@@ -316,7 +316,7 @@ $(function(){
 	console.log('*** Client Log Message: \'join_room\' payload: '+JSON.stringify(payload));
 	socket.emit('join_room',payload);
 
-	$('#quit').append('<a href="lobby.html?username='+username+' " class="btn btn-lg btn-outline-warning" role="button" aria-pressed="true">Quit</a>');
+	$('#quit').append('<a href="lobby.html?username='+username+' " class="btn btn-lg btn-outline-danger" role="button" aria-pressed="true">Quit</a>');
 
 
 });
