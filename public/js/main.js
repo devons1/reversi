40a0f2370e3637afdc14f0dba6e0774248db77fc
Devon Singh <devons1@uci.edu>
2017-06-10 17:03:12 -0700

    no message

--- public/js/main.js
+++ public/js/main.js
@@ -460,7 +460,7 @@ socket.on('game_update',function(payload){
 
 				if(payload.game.whose_turn === my_color){
 						
-						//if(payload.game.legal_moves[row][column] === my_color.substr(0,1)){
+						if(payload.game.legal_moves[row][column] === my_color.substr(0,1)){
 							$('#'+row+'_'+column).addClass('hovered_over');
 							$('#'+row+'_'+column).click(function(r,c){
 								return function(){
@@ -472,7 +472,7 @@ socket.on('game_update',function(payload){
 									socket.emit('play_token',payload);
 								};
 							}(row,column));
-						//}
+						}
 		
 				}
 				
